2d1aa014f640edf2054eb865da8ebc7df5133811
Nofear58 <santiagobentancourt@gmail.com>
2021-10-26 18:11:50 -0300

    algunas clases

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo "Bienvenidos al mundo de los pibes";
-include("conexion.php");
-?>
---
+++ php/models/Users.php
@@ -0,0 +1,77 @@
+<?php 
+    header('Content-Type: application/json');
+    include_once '.\Connection.php';
+
+    class User {
+
+        public $cedula;
+        public $password;
+        public $usuarionombre;
+        public $eshabilitado;
+        public $usuarioesactivo;
+        public $esadmin;
+
+        function __construct($cedula,$password,$usuarionombre,$eshabilitado,
+        $usuarioesactivo,$esadmin) {
+            $this->cedula=$cedula;
+            $this->password=$password;
+            $this->usuarionombre=$usuarionombre;
+            $this->eshabilitado=$eshabilitado;
+            $this->usuarioesactivo=$usuarioesactivo;
+            $this->esadmin=$esadmin;
+        }
+        
+        function getUsers(){
+            $con = Connect();
+            $sql = "SELECT * FROM usuario";
+            $result = $con->query($sql);
+            $usuarios = [];
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $usuarios[] = new User(
+                        $row["cedula"],$row["password"],$row["usuarionombre"],
+                        $row["eshabilitado"],$row["usuarioesactivo"],$row["esadmin"]);
+                }
+            } 
+            $con->close();
+            return $usuarios;
+        }
+
+        function getUserById($cedula){
+            $con = Connect();
+            $sql = "SELECT * FROM producto WHERE cedula = $cedula";
+            $result = $con->query($sql);
+            $usuarios = [];
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $usuarios[] = new User(
+                        $row["cedula"],$row["password"],$row["usuarionombre"],
+                        $row["eshabilitado"],$row["usuarioesactivo"],$row["esadmin"]);
+                }
+            } 
+            $con->close();
+            return $usuarios;
+        }
+
+        function getUserByFilter($filtro){
+            $con = Connect();
+            $sql = "SELECT * FROM producto WHERE $filtro";
+            $result = $con->query($sql);
+            $usuarios = [];
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $usuarios[] = new User(
+                        $row["cedula"],$row["password"],$row["usuarionombre"],
+                        $row["eshabilitado"],$row["usuarioesactivo"],$row["esadmin"]);
+                }
+            } 
+            $con->close();
+            return $usuarios;
+        }
+
+    }
+
+?>
