71b5a7c286313d00868d66d03e15f90022f5a805
Nofear58 <santiagobentancourt@gmail.com>
2021-10-26 22:03:19 -0300

    se deja igual que categorias

--- php/models/OrderLines.php
+++ php/models/OrderLines.php
@@ -15,79 +15,80 @@
             $this->precio=$precio;
             $this->cantidad=$cantidad;
         }
-        
-        function getOrderLines(){
-            $con = Connect();
-            $sql = "SELECT * FROM pedidolinea";
-            $result = $con->query($sql);
-            $pedidoslinea = [];
-
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    $pedidoslinea[] = new OrderLine(
-                        $row["pedidolineaid"],$row["productoid"],$row["precio"],
-                            $row["cantidad"]);
-                }
-            } 
-            $con->close();
-            return $pedidoslinea;
-        }
-
-        function getOrderLinesById($pedidolineaid){
-            $con = Connect();
-            $sql = "SELECT * FROM pedidolinea WHERE pedidolineaid = $pedidolineaid";
-            $result = $con->query($sql);
-            $pedidoslinea = [];
-
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    $pedidoslinea[] = new OrderLine(
-                        $row["pedidolineaid"],$row["productoid"],$row["precio"],
-                            $row["cantidad"]);
-                }
-            }  
-            $con->close();
-            return $pedidoslinea;
-        }
-
-        function getOrderLinesByFilter($filter){
-            $con = Connect();
-            $sql = "SELECT * FROM pedidolinea WHERE $filter";
-            $result = $con->query($sql);
-            $pedidoslinea = [];
-
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    $pedidoslinea[] = new OrderLine(
-                        $row["pedidolineaid"],$row["productoid"],$row["precio"],
-                            $row["cantidad"]);
-                }
-            }   
-            $con->close();
-            return $pedidoslinea;
-        }
-
-        function setOrderLine($pedidolinea){
-
-            $idLinea = $pedidolinea->$pedidolineaid
-            $idProd = $pedidolinea->$productoid
-            $prec = $pedidolinea->$precio
-            $cant = $pedidolinea->$cantidad
-
-            $con = Connect();
-            $sql = "INSERT INTO pedido (pedidolineaid, productoid, precio, cantidad)
-                    VALUES ($idLinea, $idProd, $prec, $cant)";
-
-            $result = $con->query($sql);
-            $con->close();
-
-            if $result == true{
-                return true;   
-            } else {
-                return false;
-            }
-        }
 
     }
+        
+    function getOrderLines(){
+        $con = Connect();
+        $sql = "SELECT * FROM pedidolinea";
+        $result = $con->query($sql);
+        $pedidoslinea = [];
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $pedidoslinea[] = new OrderLine(
+                    $row["pedidolineaid"],$row["productoid"],$row["precio"],
+                        $row["cantidad"]);
+            }
+        } 
+        $con->close();
+        return $pedidoslinea;
+    }
+
+    function getOrderLinesById($pedidolineaid){
+        $con = Connect();
+        $sql = "SELECT * FROM pedidolinea WHERE pedidolineaid = $pedidolineaid";
+        $result = $con->query($sql);
+        $pedidoslinea = [];
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $pedidoslinea[] = new OrderLine(
+                    $row["pedidolineaid"],$row["productoid"],$row["precio"],
+                        $row["cantidad"]);
+            }
+        }  
+        $con->close();
+        return $pedidoslinea;
+    }
+
+    function getOrderLinesByFilter($filter){
+        $con = Connect();
+        $sql = "SELECT * FROM pedidolinea WHERE $filter";
+        $result = $con->query($sql);
+        $pedidoslinea = [];
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $pedidoslinea[] = new OrderLine(
+                    $row["pedidolineaid"],$row["productoid"],$row["precio"],
+                        $row["cantidad"]);
+            }
+        }   
+        $con->close();
+        return $pedidoslinea;
+    }
+
+    function setOrderLine($pedidolinea){
+
+        $idLinea = $pedidolinea->$pedidolineaid
+        $idProd = $pedidolinea->$productoid
+        $prec = $pedidolinea->$precio
+        $cant = $pedidolinea->$cantidad
+
+        $con = Connect();
+        $sql = "INSERT INTO pedido (pedidolineaid, productoid, precio, cantidad)
+                VALUES ($idLinea, $idProd, $prec, $cant)";
+
+        $result = $con->query($sql);
+        $con->close();
+
+        if $result == true{
+            return true;   
+        } else {
+            return false;
+        }
+    }
+
 
 ?>
