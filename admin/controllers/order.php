450a41df821c21bdf3be8c3714aedf77f1cf75ff
Nofear58 <santiagobentancourt@gmail.com>
2021-11-03 21:36:34 -0300

    se quita codigo comentado

--- admin/controllers/order.php
+++ admin/controllers/order.php
@@ -30,48 +30,6 @@
             $this->view->render('order/detail');
             $this->view->render('footer_view');
         }
-
-        /*function add(){
-            $this->view->render('header_view');
-            $this->view->render('user/add');
-            $this->view->render('footer_view');
-        }
-
-        function add_user_db(){
-            $userName = $_POST['userName'];
-            $userId = $_POST['userId'];
-            $userPassword = $_POST['userPassword'];
-            $isAdmin = $_POST['isAdmin'];
-            echo putUser($userName,$userId,$userPassword,$isAdmin);
-
-        }
-
-        function update_user_state(){
-            $id = $_POST['id'];
-            echo updateUserState($id);
-        }
-
-        function update_user_admin_state(){
-            $id = $_POST['id'];
-            echo updateUserAdminState($id);
-        }
-
-        function edit($id){
-            $this->view->users=getUserById($id[0]);
-
-            $this->view->render('header_view');
-            $this->view->render('user/edit');
-            $this->view->render('footer_view');
-
-
-        }
-
-        function edit_user_db(){
-            $userName = $_POST['userName'];
-            $userId = $_POST['userId'];
-            $userPassword = $_POST['userPassword'];
-            echo editUser($userId,$userPassword,$userName);
-        }*/
     }
 
 ?>
